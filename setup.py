0ed858d183e707ac9a281c9cbc8bf6de26b2f567
Somtochukwu Achikanu <achikanusomtochukwu@gmail.com>
2024-06-27 22:21:53 +0100

    Initial Setup done

---
+++ setup.py
@@ -0,0 +1,28 @@
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "IBM-anti-laundering-transaction"
+AUTHOR_USER_NAME = "Somtochukwu-Achikanu"
+SRC_REPO = "IBM_anti_launder_transaction"
+AUTHOR_EMAIL = "achikanusomtochukwu@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
